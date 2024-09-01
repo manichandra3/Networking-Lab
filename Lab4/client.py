9036a2bea76104f1a4e36e32d2d806d23fbfcf18
Mani Chandra <127130821+manichandra3@users.noreply.github.com>
2024-09-01 09:59:27 +0530

    Completed assignment4 q1

--- Lab4/client.py
+++ Lab4/client.py
@@ -3,37 +3,40 @@ import time
 
 
 class Client:
-    def __init__(self, host='0.0.0.0', port=49513):
+    def __init__(self, host='0.0.0.0', port=49513, skip=3):
         self.host = host
         self.port = port
+        self.skip = skip
 
     def run_client(self):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((self.host, self.port))
         expected_sequence_number = 0
+        i = 0
 
         try:
             while True:
-                # Receive data from the server
                 frame = client_socket.recv(1024).decode('utf-8')
                 if not frame:
                     break
 
-                # Extract sequence number and data from the frame
                 sequence_number, data = frame.split(":", 1)
                 sequence_number = int(sequence_number)
 
                 print(f"Received frame with sequence number {sequence_number}: {data.strip()}")
 
-                # Check if the sequence number is as expected
-                if sequence_number != expected_sequence_number:
-                    print(f"Sending ACK for sequence number {sequence_number}")
-                    client_socket.send(str(sequence_number).encode('utf-8'))
-                    expected_sequence_number = 1 - expected_sequence_number
+                if sequence_number == expected_sequence_number:
+                    if i != self.skip:
+                        print(f"Sending ACK for sequence number {sequence_number}")
+                        client_socket.send(str(sequence_number).encode('utf-8'))
+                        expected_sequence_number = 1 - expected_sequence_number
+                    else:
+                        print(f"Skipping ACK for sequence number {sequence_number}")
                 else:
-                    # If the sequence number is not what we expect, resend the last ACK
                     print(f"Unexpected sequence number. Resending ACK for {1 - expected_sequence_number}")
                     client_socket.send(str(1 - expected_sequence_number).encode('utf-8'))
+
+                i += 1
 
         except Exception as e:
             print(f"Error: {e}")
